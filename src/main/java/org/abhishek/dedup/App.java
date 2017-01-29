7ae0e501a79171ab226d95adcc55188ad52244e0
Abhishek Dujari <abhishek.dujari@gmail.com>
2017-01-30 03:12:42 +0800

    saves stuff to file

--- src/main/java/org/abhishek/dedup/App.java
+++ src/main/java/org/abhishek/dedup/App.java
@@ -1,20 +1,31 @@
 package org.abhishek.dedup;
 
-import java.io.FileInputStream;
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.FileVisitResult;
 import java.nio.file.FileVisitor;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.attribute.BasicFileAttributes;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.Set;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import java.util.stream.Collectors;
-import net.openhft.hashing.LongHashFunction;
 
 /**
  * Hello world!
@@ -24,22 +35,57 @@ public class App {
 
     public static void main(String[] args) throws IOException {
         long sum;
+        String filename = "K:\\J_Drive.csv";
         Recurse r = new Recurse();
-        Files.walkFileTree(Paths.get("G:"), r);
-        System.out.println("Total files: " + r.getFilesCount());
+        Files.walkFileTree(Paths.get("J:\\"), r);
+        sum = r.getFilesCount();
+        System.out.println("Total files: " + sum);
+        int j = (int) Math.ceil(sum / 30000.00);
 
+        System.out.println("files split count: " + j);
         Map entries = r.getEntry();
 
+        BufferedWriter bw = new BufferedWriter(new FileWriter(filename));
         entries.forEach((name, hash) -> {
-            Set i = r.getKeysByValue(entries, hash);
-            if (i.size() > 1) {
-                System.out.print(name + ": \t");
-                i.forEach(item -> System.out.print(item + ":"));
-                System.out.println();
+            try {
+                bw.write(name + "\n");
+            } catch (IOException ex) {
+                Logger.getLogger(App.class.getName()).log(Level.SEVERE, null, ex);
             }
         }
         );
+        bw.close();
+       // splitFile(new File(filename),2);
+        
+    }
 
+    public static List<File> splitFile(File file, int sizeOfFileInMB) throws IOException {
+        int counter = 1;
+        List<File> files = new ArrayList<File>();
+        int sizeOfChunk = 1024 * 1024 * sizeOfFileInMB;
+        String eof = System.lineSeparator();
+        try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+            String name = file.getName();
+            String line = br.readLine();
+            while (line != null) {
+                File newFile = new File(file.getParent(), name + "_"
+                        + String.format("%03d", counter++));
+                try (OutputStream out = new BufferedOutputStream(new FileOutputStream(newFile))) {
+                    int fileSize = 0;
+                    while (line != null) {
+                        byte[] bytes = (line + eof).getBytes(StandardCharsets.UTF_8);
+                        if (fileSize + bytes.length > sizeOfChunk) {
+                            break;
+                        }
+                        out.write(bytes);
+                        fileSize += bytes.length;
+                        line = br.readLine();
+                    }
+                }
+                files.add(newFile);
+            }
+        }
+        return files;
     }
 
 }
@@ -63,15 +109,7 @@ class Recurse implements FileVisitor<Path> {
     @Override
     public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
         filesCount++;
-        if (Files.size(file) > 100000000L) { //100 mb
-            FileInputStream fis = new FileInputStream(file.toFile());
-            byte[] bs = new byte[204800];
-            fis.read(bs, 0, length);
-
-            long hash = LongHashFunction.xx_r39().hashBytes(bs);
-            entry.put(file.toAbsolutePath(), hash);
-        }
-
+        entry.put(file.toAbsolutePath(), 1);
         return FileVisitResult.CONTINUE;
     }
 
